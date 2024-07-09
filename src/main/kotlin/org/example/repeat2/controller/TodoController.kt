3b4721b075b026797e411ea78fd98353d9d041f9
202040303good <trmf681@gmail.com>
2024-07-09 17:00:27 +0900

    ✨ feat : Create Controller 생성

--- src/main/kotlin/org/example/repeat2/controller/TodoController.kt
+++ src/main/kotlin/org/example/repeat2/controller/TodoController.kt
@@ -1,4 +1,16 @@
 package org.example.repeat2.controller
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/todos")
 class TodoController {
+    @PostMapping
+    fun createTodo(): ResponseEntity<Boolean> {
+        return ResponseEntity.ok(true)
+    }
 }
