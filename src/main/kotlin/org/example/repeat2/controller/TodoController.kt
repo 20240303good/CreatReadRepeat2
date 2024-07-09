b10535d1a80bd47cc78899ad5b8b45405ee72331
202040303good <trmf681@gmail.com>
2024-07-09 17:20:02 +0900

    ✨ feat : Read Controller 생성

--- src/main/kotlin/org/example/repeat2/controller/TodoController.kt
+++ src/main/kotlin/org/example/repeat2/controller/TodoController.kt
@@ -1,5 +1,6 @@
 package org.example.repeat2.controller
 
+import com.sun.tools.javac.comp.Todo
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController
 class TodoController {
     @PostMapping
     fun createTodo(): ResponseEntity<Boolean> {
+        return ResponseEntity.ok(true)
+    }
+
+    @GetMapping("/{id}")
+    fun getTodo():ResponseEntity<Boolean> {
         return ResponseEntity.ok(true)
     }
 }
